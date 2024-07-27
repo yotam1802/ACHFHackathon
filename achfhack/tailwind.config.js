e143b3a40de09c4bc2623970d7a3e97d53bd8af4
Jasmit <138936262+Jasmit22@users.noreply.github.com>
2024-07-27 13:33:34 -0600

    dashboard update

--- achfhack/tailwind.config.js
+++ achfhack/tailwind.config.js
@@ -10,22 +10,14 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#006bff",
-
+          primary: "#08B4FF",
           secondary: "#836800",
-
-          accent: "#00e6ff",
-
-          neutral: "#201417",
-
-          "base-100": "#e6ffff",
-
+          accent: "#08B4FF",
+          neutral: "#08B4FF",
+          "base-100": "#FFFFFF",
           info: "#37a8ff",
-
           success: "#009900",
-
           warning: "#ffcd00",
-
           error: "#ff6570",
         },
       },
