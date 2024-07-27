4c2cfd65ef2de451e735f5e0e50dae4e48453868
yotam1802 <Yotam1802@gmail.com>
2024-07-27 12:40:03 -0600

    setup

---
+++ achfhack/src/app/(dashboard)/layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/DashboardNavbar/Navbar";
+
+export default function DashboardLayout({children}) {
+  return (
+    <div className="flex flex-col-reverse lg:flex-row min-h-full min-w-full">
+      <Navbar />
+      <div className="lg:flex-1 lg:ml-56">
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ achfhack/src/app/components/DashboardNavbar/DashboardNavbar/Navbar.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import profilePicPlaceholder from "@/assets/profile-pic-placeholder.png";
+import SignoutButton from "./SignoutButton";
+import NavbarMenu from "./NavbarMenu";
+
+export default async function Navbar() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 h-16 md:h-20 rounded-t-box menu menu-horizontal bg-base-200 lg:menu-vertical lg:rounded-box lg:min-h-screen lg:w-56 lg:shadow-md lg:p-5 gap-4 text-base text-gray-800 font-semibold tracking-tight lg:justify-between">
+      <NavbarMenu />
+      <label
+        tabIndex={0}
+        className="gap-2 btn btn-ghost text-lg font-bold invisible lg:visible"
+      >
+        <Image
+          src={user?.image || profilePicPlaceholder}
+          alt="Profile Picture"
+          width={40}
+          height={40}
+          className="w-10 rounded-full"
+        />
+        <SignoutButton />
+      </label>
+    </div>
+  );
+}
